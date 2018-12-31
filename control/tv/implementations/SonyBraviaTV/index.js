bfac0465c33f612978b34a82d59f7f032fd9f900
Christopher Hawkins <chrishawkins@fb.com>
2018-12-31 01:31:13 -0800

    Add support for setting TV source

--- control/tv/implementations/SonyBraviaTV/index.js
+++ control/tv/implementations/SonyBraviaTV/index.js
@@ -1,6 +1,41 @@
 const TcpClientManager = require(__basedir + '/util/TcpClientManager.js');
 const CommandMap = require('./CommandMap.json');
 
+const Inputs = {
+  TV: {
+    label: "TV",
+    range: 0,
+  },
+  HDMI: {
+    label: "HDMI",
+    range: 100000000,
+  },
+  SCART: {
+    label: "SCART",
+    range: 200000000,
+  },
+  COMPOSITE: {
+    label: "Composite",
+    range: 300000000,
+  },
+  COMPONENT: {
+    label: "Component",
+    range: 400000000,
+  },
+  MIRRORING: {
+    label: "Mirroring",
+    range: 500000000,
+  },
+  PC: {
+    label: "PC",
+    range: 600000000,
+  },
+  UNKNOWN: {
+    label: "Unknown",
+    range: 700000000,
+  }
+};
+
 class SonyBraviaTV {
 
   constructor(config) {
@@ -38,10 +73,49 @@ class SonyBraviaTV {
     });
   }
 
+  setVolume(volume) {
+    return this._set('AudioVolume', volume);
+  }
+
+  getInput() {
+    return this._get('Input').then(inputVal => {
+      console.log(inputVal);
+      return new Promise(resolve => {
+        if (inputVal < Inputs.HDMI.range) resolve('TV');
+        else if (inputVal < Inputs.SCART.range) resolve(Inputs.HDMI.label + ' ' + (inputVal - Inputs.HDMI.range));
+        else if (inputVal < Inputs.COMPOSITE.range) resolve(Inputs.SCART.label + ' ' + (inputVal - Inputs.SCART.range));
+        else if (inputVal < Inputs.COMPONENT.range) resolve(Inputs.COMPOSITE.label + ' ' + (inputVal - Inputs.COMPOSITE.range));
+        else if (inputVal < Inputs.MIRRORING.range) resolve(Inputs.COMPONENT.label + ' ' + (inputVal - Inputs.COMPONENT.range));
+        else if (inputVal < Inputs.PC.range) resolve(Inputs.MIRRORING.label + ' ' + (inputVal - Inputs.MIRRORING.range));
+        else if (inputVal < Inputs.UNKNOWN.range) resolve(Inputs.PC.label + ' ' + (inputVal - Inputs.PC.range));
+        else resolve(Inputs.UNKNOWN.label + inputVal);
+      });
+    });
+  }
+
+  setInput(inputValue) {
+    if (inputValue === Inputs.TV.label) {
+      return this._set('Input', 0);
+    }
+    for (var i in Inputs) {
+      console.log(i);
+      if (inputValue.startsWith(Inputs[i].label)) {
+        let number = parseInt(inputValue.substring(Inputs[i].label.length));
+        if (number != NaN) {
+          let inputId = number + Inputs[i].range;
+          return this._set('Input', inputId);
+        }
+      }
+    }
+    return new Promise((_, reject) => {
+      reject('Input not valid: ' + inputValue);
+    });
+  }
+
   _set(property, param) {
     let commandConfig = CommandMap[property];
     let paramInt = +param;
-    let paramStr = ''+paramInt;
+    let paramStr = '' + paramInt;
     let paramPadded = paramStr.padStart(16, '0');
     return this._sendCommandTCP('*SC' + commandConfig.fourCC + paramPadded);
   }
@@ -54,8 +128,11 @@ class SonyBraviaTV {
         this.filters[commandConfig.fourCC] = data => {
           if (!resolved) {
             resolved = true;
-            console.log('substr' + data.substring(7, 23));
-            resolve(parseInt(data.substring(7, 23)));
+            let substr = data.substring(7, 23);
+            if (substr === "FFFFFFFFFFFFFFFF") {
+              return reject("Unknown error");
+            }
+            return resolve(parseInt(substr));
           }
         };
         this._sendCommandTCP('*SE' + commandConfig.fourCC + paramPadded).catch(error => {
--- routes/tv.js
+++ routes/tv.js
@@ -22,18 +22,40 @@ router.post('/turnOff', function(req, res, next) {
 router.get('/volume', function(req, res, next) {
   tvImpl.getVolume().then(volume => {
     res.send('' + volume);
+  }).catch(error => {
+    res.status(500).send(error);
   });
 });
 
 router.post('/volume/:value', function(req, res, next) {
   tvImpl.setVolume(parseInt(req.params.value)).then(() => {
     res.sendStatus(200);
+  }).catch(error => {
+    res.status(500).send(error);
   });
 });
 
 router.post('/incrementVolume/:value', function(req, res, next) {
   tvImpl.incrementVolume(parseInt(req.params.value)).then(() => {
     res.sendStatus(200);
+  }).catch(error => {
+    res.status(500).send(error);
+  });
+});
+
+router.get('/input', function(req, res, next) {
+  tvImpl.getInput().then(input => {
+    res.send(input);
+  }).catch(error => {
+    res.status(500).send(error);
+  });
+});
+
+router.post('/input/:input', function(req, res, next) {
+  tvImpl.setInput(req.params.input).then(input => {
+    res.send(input);
+  }).catch(error => {
+    res.status(500).send(error);
   });
 });
 
