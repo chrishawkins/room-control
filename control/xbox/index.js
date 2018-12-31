bb6fd2c1ee5be55a434c92be1a20cf6c28029750
Christopher Hawkins <chrishawkins@fb.com>
2018-12-31 00:31:07 -0800

    Removing logging

--- control/xbox/index.js
+++ control/xbox/index.js
@@ -17,15 +17,11 @@ class Xbox {
     let powerHeader = Buffer.concat([new Buffer('dd0200', 'hex'), new Buffer(String.fromCharCode(powerPayload.length)), new Buffer('\x00\x00')]);
     let powerPacket = Buffer.concat([powerHeader, powerPayload]);
 
-    console.log(powerPayload);
-    console.log(powerHeader);
-
     return this._sendPacket(socket, powerPacket);
   }
 
   _sendPacket(socket, buffer) {
     return new Promise((resolve, reject) => {
-      console.log(buffer);
       socket.send(buffer, 0, buffer.length, Constants.xboxPort, this.ip, function(err) {
         socket.close();
         if (err) {
