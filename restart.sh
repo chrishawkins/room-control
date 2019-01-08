ecbee1f9f76a6362051e80474780dbae3120219b
Christopher Hawkins <chrishawkins@fb.com>
2019-01-07 20:39:09 -0800

    Remove lifecycle scripts, use systemd instead

--- restart.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-./kill.sh && ./run-in-background.sh
--- run-in-background.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p logs
-nohup bin/www > logs/control.out 2> logs/control.err < /dev/null &
-echo $! > .pid
